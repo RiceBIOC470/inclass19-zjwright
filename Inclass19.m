d01c62d75be3e79602d6e1dfc29f54a8c8620f4e
Aryeh <warmflasha@gmail.com>
2017-11-02 12:43:00 -0500

    Wrote Inclass19

---
+++ Inclass19.m
@@ -0,0 +1,28 @@
+%In class 19
+
+% Problem 1. Imagine a clincal trial for two drugs that are intended to lower
+% cholesterol. Assume 30 subjects are randomly divided into 3 groups that
+% either receive a placebo or one of the two treatments. The mean initials
+% levels of total cholesterol in each group are the same. The final
+% measurements look like
+
+placebo = [   194   183   199   189   189   214   212   186   191   190];
+treatment1 = [    138   217   188   126   249   217   255   196   279   123];
+treatment2 =[   152   152   151   143   161   142   142   141   161   135];
+
+%A. Make a bar plot of the means of each group with errorbars (Hint, use
+%bar and then use 'hold on' and then errorbar). 
+%B. Run a one way ANOVA analysis to see whether any of the treatment groups showed
+%a statistically signficant difference. 
+%C. use the multcompare function to look at all pairs of samples and
+%indicate which are significantly different. 
+
+%Problem 2. In this directory, you will find a .mat file with three
+%variables, xdat, ydat, and ydat2. For each pair (xdat, ydat) and (xdat,
+%ydat2), fit the data to a first, second and third order polynomial. Which
+%one is the best fit in each case? In at least one of the cases, do the
+%problem with both the polyfit/polyval functions and with the 'fit' function.
+
+
+
+
